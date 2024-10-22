c44c1e93baabf0ae80ff534aa017461a8e969a75
Rodrigo <rodrigofarii@gmail.com>
2024-10-21 21:28:34 -0600

    Call increment from dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './store/slices/counter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const { counter } = useSelector( state => state.counter)
+  const dispatch = useDispatch();
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch( increment() )}>
+          count is { counter }
         </button>
       </div>
     </>
