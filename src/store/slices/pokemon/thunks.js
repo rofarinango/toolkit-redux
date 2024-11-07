a31db24416e23157598977b5c1f7574125d68d9f
Rodrigo <rodrigofarii@gmail.com>
2024-11-06 18:29:41 -0600

    Added axios get petition pokemon api

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = ( page = 0) => {
@@ -6,6 +7,8 @@ export const getPokemons = ( page = 0) => {
 
         // TODO: realizar peticion http
 
-        // dispatch( setPokemons() );
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
+        
+        dispatch( setPokemons({pokemons: data.results, page: page + 1}) );
     }
 } 
