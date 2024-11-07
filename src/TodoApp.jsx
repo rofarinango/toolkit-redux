df56242c3bbc81762143d616f1f2e42bf3521e7d
Rodrigo <rodrigofarii@gmail.com>
2024-11-07 10:24:52 -0600

    Added RTK Query get todos

---
+++ src/TodoApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useGetTodoQuery, useGetTodosQuery } from './store/apis'
+
+export const TodoApp = () => {
+
+    const [ todoId, setTodoId ] = useState(1);
+    // const { data: todos = [], isLoading} = useGetTodosQuery();
+    const { data: todo, isLoading } = useGetTodoQuery(todoId); 
+
+    const nextTodo = () => {
+        setTodoId(todoId + 1);
+    }
+
+    const prevTodo = () => {
+        if ( todoId === 1) return;
+        setTodoId( todoId - 1);
+    }
+
+    return (
+    <>
+        <h1>Todos - RTK Query</h1>
+        <hr />
+        <h4>isLoading: { isLoading ? 'True':'False' }</h4>
+
+        <pre>{JSON.stringify(todo)}</pre>
+
+        {/* <ul>
+            { todos.map( todo => (
+                <li key={todo.id}>
+                    <strong> {todo.completed ? 'DONE: ': 'PENDING: '}</strong>
+                    {todo.title}
+                </li>
+            )) }
+        </ul> */}
+
+        <button onClick={ prevTodo }>
+            Prev To do
+        </button>
+        <button onClick={ nextTodo }>
+            Next To do
+        </button>
+    </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,14 @@ import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import { PokemonApp } from './PokemonApp.jsx'
-
+import { TodoApp } from './TodoApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp/>
+      {/* <PokemonApp/> */}
+      <TodoApp />
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
